6da2f38bbbb623d7d03c48cf80029a888ff92273
kashi6356 <kashishahir132006@gmail.com>
2024-10-13 10:49:36 +0530

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+#include<conio.h> 
+ main() 
+{ 
+int a[10],b[10],sum[10],i; 
+printf("enter the first array"); 
+for(i=0;i<10;i++) 
+ scanf("%d",&a[i]); 
+ printf("enter the second array");  
+ for(i=0;i<10;i++) 
+ scanf("%d",&a[i]); 
+ for(i=0;i<10;i++) 
+ sum[i]=a[i]+b[i]; 
+ printf("sum of two array is");  
+ for(i=0;i<10;i++) 
+ 
+ 	 printf("%d",sum[i]); 
+ 
+
+getch();  
+ } 
